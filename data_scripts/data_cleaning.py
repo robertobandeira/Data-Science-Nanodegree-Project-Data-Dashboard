51d6a6c896d356be40b1638d96549e2a8bac660c
Roberto Bandeira de Mello <roberto.bandeiram@gmail.com>
2023-12-12 14:14:13 -0500

    added API call and data manipulation + plots

--- data_scripts/data_cleaning.py
+++ data_scripts/data_cleaning.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.graph_objs as go
+import wbgapi as wb
 
 def return_figures():
     """Creates plotly visualizations
@@ -11,21 +12,85 @@ def return_figures():
         list (dict): list containing the plotly visualizations
 
     """
-    graph_one = []    
-    graph_one.append(
-      go.Scatter(
-      x = [0, 1, 2, 3, 4, 5],
-      y = [0, 2, 4, 6, 8, 10],
-      mode = 'lines'
-      )
-    )
+    data = wb.data.DataFrame(['NY.GDP.PCAP.CD',
+                              'SP.POP.TOTL',
+                              'NY.GDP.MKTP.CD', 
+                              'SE.XPD.TOTL.GD.ZS'], 
+                              ['USA', 'BRA', 'CHN', 'IND'], 
+                              columns='series').reset_index()
+    data.rename(columns={'NY.GDP.PCAP.CD':'GDP Per Capita', 
+                 'SP.POP.TOTL':'Population', 
+                 'NY.GDP.MKTP.CD':'GDP', 
+                 'SE.XPD.TOTL.GD.ZS':'Education Expenditure'}, inplace=True)
+    data['time'] = data.time.str.extract('(\d+)')
+    print(data.head())
 
-    layout_one = dict(title = 'Chart One',
-                xaxis = dict(title = 'x-axis label'),
-                yaxis = dict(title = 'y-axis label'),
+    graph_one = []
+    graph_two = []
+    graph_three = []
+    graph_four = []
+
+    for country in data.economy.unique():
+        data_country = data[data.economy == country]
+        graph_one.append(
+            go.Scatter(
+                x = data_country.time,
+                y = data_country['Population'],
+                mode = 'lines',
+                name = country
+            )
+        )
+
+        graph_two.append(
+            go.Scatter(
+                x = data_country.time,
+                y = data_country['GDP'],
+                mode = 'lines',
+                name = country
+            )
+        )
+
+        graph_three.append(
+            go.Scatter(
+                x = data_country.time,
+                y = data_country['GDP Per Capita'],
+                mode = 'lines',
+                name = country
+            )
+        )
+
+        graph_four.append(
+            go.Scatter(
+                x = data_country.time,
+                y = data_country['Education Expenditure'],
+                mode = 'lines',
+                name = country
+            )
+        )
+
+    layout_one = dict(title = 'Population',
+                xaxis = dict(title = 'Year'),
+                yaxis = dict(title = 'Population'),
+                )
+    
+    layout_two = dict(title = 'GDP',
+                xaxis = dict(title = 'Year'),
+                yaxis = dict(title = 'GDP'),
+                )
+    
+    layout_three = dict(title = 'GDP Per Capita',
+                xaxis = dict(title = 'Year'),
+                yaxis = dict(title = 'GDP Per Capita'),
+                )
+    
+    layout_four = dict(title = 'Education Expenditure',
+                xaxis = dict(title = 'Year'),
+                yaxis = dict(title = 'Education Expenditure'),
                 )
 
     figures = []
     figures.append(dict(data=graph_one, layout=layout_one))
-
+    figures.append(dict(data=graph_two, layout=layout_two))
+    figures.append(dict(data=graph_three, layout=layout_three))
+    figures.append(dict(data=graph_four, layout=layout_four))
     return figures
